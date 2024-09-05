76dc8a1fef5c1a4a46d97364223b1e32270897b9
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-09-06 04:30:47 +0530

    User signin

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -42,7 +42,34 @@ const PatientReister = catchAsyncErrors(async (req, res, next) => {
     });
     res.status(200).json({
         sucess: true,
-        message: "Registeration sucessfully"
+        message: "User Registered Sucessfully"
     });
 });
 module.exports = PatientReister;
+
+const login=catchAsyncErrors(async(req,res,next)=>{
+    const{Email,Password,confirmPassword,role}=req.body;
+    if(!Email || !Password || !confirmPassword || !role){
+        return next(new Errorhandler("Please Provide all details",400));
+    };
+    if(Password !== confirmPassword){
+        return next(new Errorhandler("Password and Confirm password Do Not Matched",400));
+    }
+    const user= await User.findOne({Email}).select('+Password');
+    if(!user){
+        return next(new Errorhandler("Invalid Password or Email",400));
+    }
+    const isconfirmPassword=await user.confirmPassword();
+    if(!isconfirmPassword){
+        return next(new Errorhandler("Invalid Password or Email",400));
+    }
+    if(role !== user.role){
+        return next(new Errorhandler("User with this role not Found!",400));
+    }
+    res.status(200).json({
+        sucess:true,
+        message:"User Logged in sucessfully",
+    });
+});
+
+module.exports=login;
