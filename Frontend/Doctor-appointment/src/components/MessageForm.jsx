5f40ec0783bd6e27bd971a1bf7bab3b16771f08e
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-09-08 02:05:42 +0530

    code add

--- Frontend/Doctor-appointment/src/components/MessageForm.jsx
+++ Frontend/Doctor-appointment/src/components/MessageForm.jsx
@@ -14,7 +14,7 @@ const MessageForm = () => {
   };
 
   return (
-    <Container fluid style={{ backgroundColor: "#10E5D0", maxHeight: "100%", maxWidth: "100%" }} className='p-2'>
+    <Container fluid style={{ backgroundColor: "#10E5D0", maxHeight: "100%", maxWidth: "100%" }}>
       <div className='message animate__animated animate__fadeInRight'>
         <h2 className='text-center text-black fw-1' style={{ fontSize: "30px", fontWeight: "bold" }}>Send Us A Message</h2>
         <Form onSubmit={handlechange}>
