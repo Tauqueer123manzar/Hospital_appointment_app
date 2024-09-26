4072280d5b4f42cdc4ce55074c811f9a615b19b6
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-09-26 22:19:45 +0530

    code add

--- Backend/controllers/messagecontroller.js
+++ Backend/controllers/messagecontroller.js
@@ -1,25 +1,3 @@
-// const Message = require('../models/message');
-// const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
-// const Errorhandler = require("../middlewares/Errormiddleware");
-// const sendmessage = catchAsyncErrors(async (req, res, next) => {
-//     const { Firstname, Lastname, Email, Phonenumber, Message: userMessage } = req.body;
-
-//     if (!Firstname || !Lastname || !Email || !Phonenumber || !userMessage) {
-//         return next(new Errorhandler("please fill the full form", 400));
-//     }
-//     try {
-//         await Message.create({ Firstname, Lastname, Email, Phonenumber, Message: userMessage });
-//         res.status(200).json({
-//             success: true,
-//             message: "Message sent successfully",
-//         });
-//     } catch (error) {
-//         next(error); 
-//     }
-// });
-
-// module.exports = sendmessage;
-
 const Message=require("../models/message");
 const catchAsyncErrors=require("../middlewares/catchAsyncErrors");
 const Errorhandler=require("../middlewares/Errormiddleware");
@@ -28,10 +6,14 @@ const sendmessage=catchAsyncErrors(async(req,res,next)=>{
     if(!firstname || !lastname ||!email || !phonenumber ||!message){
         return next(new Errorhandler("Please fill full form",400));
     }
-    await Message.create({firstname,lastname,email,phonenumber,message});
-    res.status(200).json({
-        success:true,
-        message:"Message sent successfully"
-    });
+    try{
+        await Message.create({firstname,lastname,email,phonenumber,message});
+        res.status(200).json({
+            success:true,
+            message:"Message sent successfully"
+        });
+    }catch(error){
+        next(error);
+    }
 });
 module.exports=sendmessage;
