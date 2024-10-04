7e52921e2d9ee04facd598eb3f02a73f4815181a
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-10-04 17:23:22 +0530

    newadmin add code

--- Backend/router/UserRouter.js
+++ Backend/router/UserRouter.js
@@ -1,7 +1,9 @@
-const {PatientRegister,loginRegister} = require("../controllers/userController");
+const {PatientRegister,loginRegister,addnewadmin} = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
 
 router.post("/patient/register", PatientRegister);
 router.post("/login",loginRegister);
+router.post("/admin/addnew",addnewadmin);
+
 module.exports = router
