f3101bf7afc733801bbd669421e9e9ff3cdb443d
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-10-17 18:21:57 +0530

    Admin Logout & Patient Logout & Get All doctors

--- Backend/router/UserRouter.js
+++ Backend/router/UserRouter.js
@@ -1,4 +1,4 @@
-const {PatientRegister,loginRegister,addnewAdmin} = require("../controllers/userController");
+const {PatientRegister,loginRegister,addnewAdmin, getallDoctors, getUserDetails, adminLogout, patientLogout} = require("../controllers/userController");
 const express = require("express");
 const router = express.Router();
 const {isAdminAuthenticated,isPatientAuthenticated}=require("../middlewares/auth");
@@ -6,5 +6,10 @@ const {isAdminAuthenticated,isPatientAuthenticated}=require("../middlewares/auth
 router.post("/patient/register", PatientRegister);
 router.post("/login",loginRegister);
 router.post("/admin/addnew",isAdminAuthenticated,addnewAdmin);
+router.get("/doctors",getallDoctors);
+router.get("/admin/me",isAdminAuthenticated,getUserDetails);
+router.get("/patient/me",isPatientAuthenticated,getUserDetails);
+router.get("/admin/logout",isAdminAuthenticated,adminLogout);
+router.get("/patient/logout",isPatientAuthenticated,patientLogout);
 
 module.exports = router
