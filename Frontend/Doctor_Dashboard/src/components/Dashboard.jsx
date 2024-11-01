ec3b3a7fb68b1a4931f3c58dc660648f966078b0
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-11-01 23:34:50 +0530

    code add

--- Frontend/Doctor_Dashboard/src/components/Dashboard.jsx
+++ Frontend/Doctor_Dashboard/src/components/Dashboard.jsx
@@ -1,18 +1,12 @@
-import React, { useEffect,useContext }  from 'react'
-import { useNavigate } from 'react-router-dom';
-import { context } from '../main'
+import React from 'react'
+// import React, { useEffect,useState, useContext }  from 'react'
+// import { useNavigate } from 'react-router-dom';
+// import { context } from '../main'
 import '../App.css'
 import { FaUsers } from "react-icons/fa";
 import { FaCalendarAlt} from 'react-icons/fa';
 import { FaCheck} from 'react-icons/fa';
 const Dashboard = () => {
-  const { isAuthenticated, setIsAuthenticated } = useContext(context);
-  const navigateTo = useNavigate();
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigateTo('/login');
-    }
-  })
   return (
     <div style={{backgroundColor:"rgb(211, 211, 211)",Width:"100%",height:"99vh",top:"0"}}>
       <div className='values'>
@@ -21,7 +15,7 @@ const Dashboard = () => {
        <div className='val-box'>
        <i style={{fontSize:"35px",fontWeight:"700",marginLeft:"20px"}}><FaUsers style={{color:"blue"}}/></i>
         <div>
-          <span>New Users</span>
+          <span>Earning</span>
           <h3>10,000</h3>
         </div>
        </div>
@@ -30,15 +24,15 @@ const Dashboard = () => {
        <i style={{fontSize:"35px",fontWeight:"700",marginLeft:"20px"}}><FaCalendarAlt color="pink" /></i>
         <div>
           <span>Appointment</span>
-          <h3>8,267</h3>
+          <h3>6</h3>
         </div>
        </div>
 
        <div className='val-box'>
        <i style={{fontSize:"35px",fontWeight:"700",marginLeft:"20px"}}> <FaCheck color="purple" /></i>
         <div>
-          <span>Confirm Booking</span>
-          <h3>4,000</h3>
+          <span>Patient</span>
+          <h3>30</h3>
         </div>
        </div>
 
