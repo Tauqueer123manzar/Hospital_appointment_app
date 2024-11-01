b9488f0525c31e43566a155729c883bf4e3042c2
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-11-02 03:05:40 +0530

    code add

--- Frontend/Doctor_Dashboard/src/components/Dashboard.jsx
+++ Frontend/Doctor_Dashboard/src/components/Dashboard.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-// import React, { useEffect,useState, useContext }  from 'react'
-// import { useNavigate } from 'react-router-dom';
-// import { context } from '../main'
+import React, { useEffect,useState, useContext }  from 'react'
+import { useNavigate } from 'react-router-dom';
+import { context } from '../main'
 import '../App.css'
 import { FaUsers } from "react-icons/fa";
 import { FaCalendarAlt} from 'react-icons/fa';
 import { FaCheck} from 'react-icons/fa';
 const Dashboard = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(context);
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigateTo('/login');
+    }
+  },);
+
   return (
     <div style={{backgroundColor:"rgb(211, 211, 211)",Width:"100%",height:"99vh",top:"0"}}>
       <div className='values'>
-      <h1>Dashboard</h1>
+      <h1 className="" style={{fontWeight:"550",fontFamily:"initial"}}>Doctor Dashboard</h1>
       <div className='value'>
        <div className='val-box'>
        <i style={{fontSize:"35px",fontWeight:"700",marginLeft:"20px"}}><FaUsers style={{color:"blue"}}/></i>
