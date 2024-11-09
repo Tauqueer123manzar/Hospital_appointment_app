081eb95b91e137e67776648ac7852a655c6afa16
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-11-09 22:39:57 +0530

    Api fetching from backend to frontend

--- Frontend/src/components/Doctors.jsx
+++ Frontend/src/components/Doctors.jsx
@@ -3,11 +3,14 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { FaCircle } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const Doctors = ({ doctor }) => { 
+const Doctors = () => {
   const navigate = useNavigate();
-  
+  const [doctors, setDoctors] = useState([]);
   const [visibleDoctors, setVisibleDoctors] = useState(12);
+  const [selectedDepartment,setSelectedDepartment]=useState("");
 
   // Function to handle navigation to the doctor's profile.
   const handleDoctorClick = (id) => {
@@ -15,9 +18,37 @@ const Doctors = ({ doctor }) => {
   };
 
   // Function to show more doctors.
-  const handleShowMore=()=>{
+  const handleShowMore = () => {
     setVisibleDoctors(prev => prev + 12)
   }
+
+  const handleDepartmentChange=(event)=>{
+    setSelectedDepartment(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchDoctorData = async () => {
+      try {
+
+        const response = await axios.get("http://localhost:8080/api/v1/user/doctors", {
+          headers: {
+            "Content-Type": "multipart/form-data",
+             Authorization: `Bearer ${localStorage.getItem("adminToken")}`
+          },
+        });
+        setDoctors(response.data.doctors);
+        console.log(response.data.doctors);
+      } catch (error) {
+        console.log(error.response.data.message);
+      }
+    }
+    fetchDoctorData();
+  }, []);
+
+  // filtereddoctors
+  const filteredDoctors=selectedDepartment?
+  doctors.filter(doctors => doctors.doctordepartment===selectedDepartment):
+  doctors;
 
   return (
     <Container fluid>
@@ -28,24 +59,29 @@ const Doctors = ({ doctor }) => {
         <p className="text-center" style={{ fontFamily: "initial" }}>
           Simply browse through our extensive list of trusted doctors.
         </p>
-        {doctor.slice(0, visibleDoctors).map((doctorItem) => (
-          <Col xs={12} sm={6} md={4} lg={2} className="p-3" key={doctorItem.id}>
-            <Card className="doctorcard" onClick={() => handleDoctorClick(doctorItem.id)} style={{ cursor: "pointer",height:"350px"}}>
-              <div className="image-wrapper">
-                <Card.Img variant="top" src={doctorItem.image} className="doctorimage" />
-              </div>
+        {doctors.slice(0, visibleDoctors).map((doctorItem) => (
+          <Col xs={12} sm={6} md={4} lg={3} className="p-3" key={doctorItem.id}>
+            <Card className="doctorcard" onClick={() => handleDoctorClick(doctorItem.id)} style={{ cursor: "pointer", height: "450px"}}>
+            <Card.Img
+                      variant="top"
+                      src={doctorItem.docAvatar ? doctorItem.docAvatar.url : 'fallback-image-url'} // Fallback image if avatar is missing
+                      alt='Doctor Avatar'
+                      style={{height:'300px',width:"100%"}}
+                    />
               <Card.Body>
                 <Card.Subtitle style={{ color: 'lightgreen', padding: "5px" }}>
-                  <FaCircle style={{ fontSize: "10px" }} /> {doctorItem.title}
+                  <FaCircle style={{ fontSize: "10px"}} /><span className='ms-2'>{doctorItem.firstname} {doctorItem.lastname}</span>
                 </Card.Subtitle>
-                <Card.Subtitle>{doctorItem.name}</Card.Subtitle>
-                <Card.Title>{doctorItem.description}</Card.Title>
+                <Card.Subtitle style={{fontFamily:"initial"}}><span>Email: </span>{doctorItem.email}</Card.Subtitle>
+                <Card.Subtitle  style={{fontFamily:"initial"}}><span>Phone: </span>{doctorItem.phone}</Card.Subtitle>
+                <Card.Subtitle  style={{fontFamily:"initial"}}><span>Gender: </span>{doctorItem.gender}</Card.Subtitle>
+                <Card.Subtitle  style={{fontFamily:"initial"}}><span>Department: </span>{doctorItem.doctordepartment}</Card.Subtitle>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
-      {visibleDoctors < doctor.length && (
+      {visibleDoctors < doctors.length && (
         <div className="text-center mt-4 mb-4">
           <Button variant="primary" onClick={handleShowMore}>
             Show More
