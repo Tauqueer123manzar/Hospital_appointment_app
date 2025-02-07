7ec62dfa889a0685bf478cbec29f638b23d221f0
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-02-08 01:13:31 +0530

    code added

--- Backend/models/Signin.js
+++ Backend/models/Signin.js
@@ -9,10 +9,10 @@ const UserSchema= new mongoose.Schema({
         type:String,
         required:true
     },
-    confirmPassword:{
+    confirmPassword:{  
         type:String,
         required:true
-    }
+    },
 });
 
 const User= mongoose.model("User",UserSchema);
