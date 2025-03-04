469531154b1fceadd50f54093f2cc5a35398397d
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-03-05 00:50:35 +0530

    Appointment created successfully

--- Backend/models/AppointmentSchema.js
+++ Backend/models/AppointmentSchema.js
@@ -1,54 +1,61 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const User = require("../models/UserSchema");
 
-const AppointmentSchema = new mongoose.Schema({
+const appointmentSchema = new mongoose.Schema({
     patientName: {
         type: String,
         required: true,
         trim: true
     },
-    specialization: {
+    phone: {
         type: String,
         required: true,
-        trim: true 
+        minLength: [10, "Phone number must be at least 10 characters"],
+        maxLength: [12, "Phone number can be at most 12 characters"]
     },
-    appointmentDate: {
-        type: Date,
+    gender: {
+        type: String,
+        required: true,
+        enum: ["Male", "Female"]
+    },
+    appointment_date: {
+        type: String,
         required: true
     },
-    hasvisited:{
-        type:Boolean,
-        default:false
-    },
-   doctorName:{
-       type:String,
-       required:true
-   },
-    selectTime: {
+    department: {
         type: String,
-        required: true,
-        trim: true,
-        enum:["9:00 AM","10:00 AM","11:00 AM","1:00 PM","2:00 PM","3:00 PM"]
+        required: true
+    },
+    doctor: {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        }
     },
     doctorId: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
-        required: true,
-        index:true
+        required: true
     },
     patientId: {
         type: mongoose.Schema.ObjectId,
         ref: "User",
-        required: true,
-        index:true
+        required: true
+    },
+    hasVisited: {
+        type: Boolean,
+        default: false
     },
     status: {
         type: String,
         enum: ["Pending", "Accepted", "Rejected"],
         default: "Pending"
-    },
+    }
 }, { timestamps: true });
 
-const Appointment = mongoose.model("Appointment", AppointmentSchema);
+const Appointment = mongoose.model("Appointment", appointmentSchema);
 module.exports = Appointment;
